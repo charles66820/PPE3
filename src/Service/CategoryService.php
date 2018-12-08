26a8aced0917efac3998f188ade5d6b12a223b0b
charles66820 <charles.goedefroit@gmail.com>
2018-12-08 22:21:18 +0100

    category service for twig globals var, print category in nav bar and graded button in menu

---
+++ src/Service/CategoryService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use App\Entity\Category;
+
+class CategoryService
+{
+    private $manager;
+    public function __construct(RegistryInterface $registry)
+    {
+        $this->manager = $registry->getManagerForClass(Category::class);
+
+    }
+    public function getAllCategorys()
+    {
+        return $this->manager->getRepository(Category::class)
+            ->findAll();
+    }
+}
