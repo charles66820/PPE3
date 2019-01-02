c7ca547a7330373ffd07c5b7b629e12dce34aaf8
charles66820 <charles.goedefroit@gmail.com>
2019-01-02 21:03:01 +0100

    les photos produits fonctionne

--- src/Entity/ProductPicture.php
+++ src/Entity/ProductPicture.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * ProductPicture
  * 
  * @ORM\Table(name="product_picture", uniqueConstraints={@ORM\UniqueConstraint(name="FK_productPicture_picture", columns={"picture_name"})}, indexes={@ORM\Index(name="FK_productPicture_idProduct", columns={"id_product"})})
  * @ORM\Entity
+ * @Vich\Uploadable
  */
 class ProductPicture
 {
@@ -36,6 +39,18 @@ class ProductPicture
      */
     private $product;
 
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $updatedAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -61,6 +76,47 @@ class ProductPicture
     public function setProduct(?Product $product): self
     {
         $this->product = $product;
+
+        return $this;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+    public function getImage()
+    {
+        return $this->pictureName;
+    }
+
+    public function setImage($pictureName): self
+    {
+        $this->pictureName = $pictureName;
 
         return $this;
     }
