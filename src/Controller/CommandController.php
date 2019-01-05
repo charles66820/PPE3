b8303101e110fda0e8bc2dcf302ed260747706fa
charles66820 <charles.goedefroit@gmail.com>
2019-01-06 00:13:29 +0100

    les commandes est le service paypal

--- src/Controller/CommandController.php
+++ src/Controller/CommandController.php
@@ -3,9 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Address;
+use App\Services\PayPalService;
+use Doctrine\ORM\EntityRepository;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\RedirectUrls;
+use PayPal\Exception\PayPalConnectionException;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommandController extends AbstractController
@@ -13,7 +21,7 @@ class CommandController extends AbstractController
     /**
      * @Route("/order", name="order")
      */
-    public function getOrder()
+    public function getOrder(Request $request)
     {
         $leClient = $this->getUser();
         if($leClient == null){
@@ -30,6 +38,11 @@ class CommandController extends AbstractController
                 ],
                 'required' => true,
                 'class' => Address::class,
+                'query_builder' => function (EntityRepository $er) use ($leClient) {
+                    return $er->createQueryBuilder('a')
+                        ->where('a.client = :idcli')
+                        ->setParameter('idcli', $leClient->getId());
+                },
                 'choice_label' => 'address',
                 'attr' => [
                     'class' => 'form-control',
@@ -43,9 +56,143 @@ class CommandController extends AbstractController
                 ],
             ])
             ->getForm();
-        return $this->render('command/order.html.twig', [
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $idDeliveryAddress = $request->request->get('chooseAddress')['deliveryAddress'];
+            $deliveryAddressValid = false;
+            foreach ($leClient->getAddress() as $address) {
+                if ($address->getId() == $idDeliveryAddress) $deliveryAddressValid = true;
+            }
+
+            if ($idDeliveryAddress == null || !$deliveryAddressValid) {
+                return $this->render('market/order.html.twig', [
+                    'title' => 'Commander',
+                    'form' => $form->createView(),
+                    'notifOrder' => [
+                        'msg' => 'Erreur avec l\'adresse de livraison',
+                        'class' => 'alert-danger',
+                    ]
+                ]);
+            }
+
+            $address = $this->getDoctrine()
+                ->getRepository(Address::class)
+                ->find($idDeliveryAddress);
+
+            //url
+            $redirectUrls = new RedirectUrls();
+            $redirectUrls->setReturnUrl($this->getParameter('completUrl').$this->generateUrl('paymentdone'));
+            $redirectUrls->setCancelUrl($this->getParameter('completUrl').$this->generateUrl('paymentcancel'));
+
+            //payment method
+            $payer = new Payer();
+            $payer->setPaymentMethod("paypal");
+
+            $apiContext = PayPalService::getPayPalApiContext();
+            $transaction = PayPalService::getPayPalTransaction($leClient, $address);
+
+            $transaction->setCustom(JSON_encode([
+                'idAddress' => $idDeliveryAddress,
+            ]));
+
+            // payment
+            $payment = new Payment();
+            $payment->setIntent('sale');
+            $payment->setPayer($payer);
+            $payment->setRedirectUrls($redirectUrls);
+            $payment->setTransactions([$transaction]);
+
+            try {
+                $payment->create($apiContext);
+                // echo json_encode([
+                //   'id' => $payment->getId()
+                // ]);
+                return $this->redirect($payment->getApprovalLink());
+            } catch (PayPalConnectionException $e) {
+                if ($this->getParameter('kernel.environment') == "dev"){
+                    dump($e->getData());die();
+                }
+                return $this->render('market/order.html.twig', [
+                    'title' => 'Commander',
+                    'form' => $form->createView(),
+                    'notifOrder' => [
+                        'msg' => 'Erreur avec lors du paiment',
+                        'class' => 'alert-danger',
+                    ]
+                ]);
+            } catch (\Exception $e) {
+                if ($this->getParameter('kernel.environment') == "dev"){
+                    dump($e);die();
+                }
+                return $this->render('market/order.html.twig', [
+                    'title' => 'Commander',
+                    'form' => $form->createView(),
+                    'notifOrder' => [
+                        'msg' => 'Erreur avec lors du paiment',
+                        'class' => 'alert-danger',
+                    ]
+                ]);
+            }
+        }
+
+        return $this->render('market/order.html.twig', [
             'title' => 'Commander',
             'form' => $form->createView(),
+            'notifOrder' => null,
         ]);
     }
+
+    /**
+     * @Route("/command/paymentdone", name="paymentdone")
+     */
+    public function getPaymentDone(Request $request)
+    {
+        $leClient = $this->getUser();
+        $apiContext = PayPalService::getPayPalApiContext();
+        $transaction = PayPalService::getPayPalPayment($leClient);
+
+        $payment = Payment::get($_GET["paymentId"], $apiContext);
+
+        $execution = new PaymentExecution();
+        $execution->setPayerID($_GET["PayerID"]);
+
+        $execution->setTransactions([$transaction]);
+
+        try {
+            $payment->execute($execution, $apiContext);
+        } catch (PayPalConnectionException $e) {
+            if ($this->getParameter('kernel.environment') == "dev"){
+                dump($e->getData());die();
+            }
+        } catch (\Exception $e) {
+            if ($this->getParameter('kernel.environment') == "dev"){
+                dump($e);die();
+            }
+        }
+        dump('fini');die();
+//        //add commande in bdd
+//        //get custome data don idaddress
+//        $custom = JSON_decode($payment->getTransactions()[0]->getCustom());
+//        $payment->getTransactions()[0]->getAmount()->getTotal();
+//        $payment->getTransactions()[0]->getAmount()->getDetails()->getShipping();
+//        $custom->IDAdresse;
+//        $contenus = $payment->getTransactions()[0]->getItemList()->getItems();
+//        foreach ($contenus as $unContenu) {
+//            $unContenu->getPrice(); //PrixUnitaire
+//            $unContenu->getQuantity(); //QuantiteContenu
+//        }
+//        //supprimePanier();
+    }
+
+    /**
+     * @Route("/command/paymentcancel", name="paymentcancel")
+     */
+    public function getPaymentCancel(Request $request)
+    {
+        return new Response(
+            '<html><body>Commende annuler</body></html>'
+        );
+    }
+
 }
--- src/Services/PayPalService.php
+++ src/Services/PayPalService.php
@@ -1,18 +1,27 @@
 <?php
 namespace App\Services;
 
+use App\Entity\Address;
+use App\Entity\Client;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\ShippingAddress;
+use PayPal\Api\Transaction;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class PayPalService
 {
-    private static $manager;
     private static $container;
     public function __construct(ContainerInterface $container)
     {
         self::$container = $container;
     }
 
-    public static function getPayPalPayment()
+    public static function getPayPalApiContext()
     {
         $apiContext = new ApiContext(
             new OAuthTokenCredential(
@@ -20,5 +29,52 @@ class PayPalService
                 getenv('PAYPALSECRET')
             )
         );
+        return $apiContext;
+    }
+
+    public static function getPayPalTransaction(Client $leClient, Address $address)
+    {
+
+        //adresse de livraision
+        $shippingAddress = new ShippingAddress();
+        $shippingAddress->setLine1($address->getWay());
+        $shippingAddress->setLine2($address->getComplement());
+        $shippingAddress->setCity($address->getCity());
+        $shippingAddress->setCountryCode("FR");
+        $shippingAddress->setPostalCode($address->getZipCode());
+
+        // liste des elements du panier
+        $list = new ItemList();
+        $list->setShippingAddress($shippingAddress);
+
+        foreach ($leClient->getCartLines() as $cartLine) {
+            $item = new Item();
+            $item->setSku($cartLine->getProduct()->getReference());
+            $item->setName($cartLine->getProduct()->getTitle());
+            $item->setPrice($cartLine->getProduct()->getUnitPriceHT());
+            $item->setCurrency("EUR");
+            $item->setQuantity($cartLine->getQuantity());
+
+            $list->addItem($item);
+        }
+
+        $details = new Details();
+        $details->setShipping(10);
+        $details->setTax($leClient->getTotalPrice()-$leClient->getTotalPriceHT());
+        $details->setSubTotal($leClient->getTotalPriceHT());
+
+        // payment amount
+        $amount = new Amount();
+        $amount->setCurrency("EUR");
+        $amount->setTotal($leClient->getTotalPrice()+10);
+        $amount->setDetails($details);
+
+        // transaction
+        $transaction = new Transaction();
+        $transaction->setItemList($list);
+        $transaction->setDescription("Achat sur le site de vente en ligne Ô'Tako");
+        $transaction->setAmount($amount);
+
+        return $transaction;
     }
 }
