e11a3968fd4fad0179329e50fb78931db41c7893
charles66820 <charles.goedefroit@gmail.com>
2019-01-06 16:41:59 +0100

    commendes in easy admin

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
