8e834da7a02b9abab890855a3ec46ed9c4ce03d0
charles66820 <charles.goedefroit@gmail.com>
2019-03-30 18:24:34 +0100

    test de surcharge

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -24,7 +24,17 @@ class ProductRepository extends ServiceEntityRepository
         $conn = $this->getEntityManager()->getConnection();
         $execParam = [];
 
-        $sql = 'SELECT DISTINCT p.id_product as id, p.product_title as title, p.unit_price_HT as unitPriceHT, (SELECT pp.picture_name FROM product_picture as pp WHERE p.id_product = pp.id_product LIMIT 1 OFFSET 0) as picture, ROUND(AVG(o.score), 1) as avgstars FROM opinion as o RIGHT JOIN product as p ON o.id_product = p.id_product ';
+        $sqlStart = 'SELECT DISTINCT p.id_product as id, p.product_title as title, p.unit_price_HT as unitPriceHT, (SELECT pp.picture_name FROM product_picture as pp WHERE p.id_product = pp.id_product LIMIT 1 OFFSET 0) as picture, po.avgstars ';
+        $sqlCount = 'SELECT COUNT(*) ';
+        $sql = 'FROM product as p, (SELECT pr.id_product as id, ROUND(AVG(o.score), 1) as avgstars FROM opinion as o RIGHT JOIN product as pr ON o.id_product = pr.id_product GROUP BY pr.id_product ';
+
+        if ($stars !== null && $stars > 0 && $stars < 6) {
+            $sql .= 'HAVING avgstars >= :stars ';
+            $execParam['stars'] = $stars;
+        }
+
+        $sql .= ') as po ';
+
         if ($cat !== null && $q !== null) {
             $sql .= 'WHERE p.id_category = (SELECT DISTINCT c.id_category FROM category as c WHERE c.name_category = :cat ) AND (p.product_title LIKE :q OR p.description LIKE :q ) ';
             $execParam['cat'] = $cat;
@@ -35,14 +45,13 @@ class ProductRepository extends ServiceEntityRepository
         } else if ($q !== null) {
             $sql .= 'WHERE (p.product_title LIKE :q OR p.description LIKE :q ) ';
             $execParam['q'] = '%'.$q.'%';
+        } else {
+            $sql .= 'WHERE 1 ';
         }
 
-        $sql .= 'GROUP BY p.id_product, p.product_title, p.unit_price_HT ';
+        $sql .= 'AND po.id = p.id_product ';
 
-        if ($stars !== null && $stars > 0 && $stars < 6) {
-            $sql .= 'HAVING avgstars >= :stars ';
-            $execParam['stars'] = $stars;
-        }
+        $sqlCount = $sqlCount.$sql;
 
         if ($mprice == 'DESC') {
             $sql .= 'ORDER BY unit_price_HT, product_title DESC ';
@@ -52,14 +61,13 @@ class ProductRepository extends ServiceEntityRepository
             $sql .= 'ORDER BY product_title ASC ';
         }
 
-        $sqlCount = $sql;
         $page -=1;
         if ($page !== null && $page !== '') {
             $sql .= 'LIMIT 20 OFFSET '.(20 * abs($page)).' ';
         }
         $conn->beginTransaction();
 
-        $stmt = $conn->prepare($sql);
+        $stmt = $conn->prepare($sqlStart.$sql);
         $index = 0;
         foreach ($execParam as $p) {
             $stmt->bindValue(array_keys($execParam)[$index++], $p);
